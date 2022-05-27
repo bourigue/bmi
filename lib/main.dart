851e00155645a5b1734b428ad4746eb1d89af437
bourigue <rrbourigue@gmail.com>
2022-05-27 15:03:53 +0100

    add snackbar bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( myapp() );
+  runApp( Test() );
             }
 
 class myapp extends StatelessWidget{
@@ -27,6 +28,60 @@ class myapp extends StatelessWidget{
   }
 
 }
+//snackbar
+class Test extends StatefulWidget{
+  Test({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return TestState();
+  }
+
+}
+
+class TestState extends State<Test>{
+
+  GlobalKey<ScaffoldState> keysnk=new GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(
+         title: Text('SnackBar Playground'),
+       ),
+       body: Builder(
+         builder: (context) =>
+             Center(
+               child: RaisedButton(
+                 color: Colors.pink,
+                 textColor: Colors.white,
+                 onPressed: (){
+
+                   final snackBar = SnackBar(
+                     elevation: 0,
+                     behavior: SnackBarBehavior.floating,
+                     backgroundColor: Colors.transparent,
+                     content: AwesomeSnackbarContent(
+                       title: 'On Snap!',
+                       message:
+                       'This is an example error message that will be shown in the body of snackbar!',
+                       contentType: ContentType.failure,
+                     ),
+                   );
+                   Scaffold.of(context).showSnackBar(snackBar);
+                 },
+                 child: Text('Display SnackBar'),
+               ),
+             ),
+       ),
+     ),
+   );
+  }
+
+}
+_displaySnackBar(BuildContext context) {
+
+}
+
 
 
 
