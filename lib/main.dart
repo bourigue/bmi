9af8b716028cc2857b84200662ea0f3fc6e6f9b0
bourigue <rrbourigue@gmail.com>
2022-05-27 10:51:22 +0100

    column

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp( myapp() );
-}
+            }
 
 class myapp extends StatelessWidget{
   @override
@@ -13,44 +13,12 @@ class myapp extends StatelessWidget{
 
       ),
       body:Container(
+       child:Column(children: [InkWell(
+           onTap:(){
 
 
-        decoration:BoxDecoration(
-
-            gradient:LinearGradient(begin:Alignment.topCenter,
-                                    end:Alignment.bottomCenter,
-                                    colors: [
-                                      Colors.red.shade300,
-                                      Colors.red.shade400,
-                                      Colors.red.shade500,
-                                      Colors.red.shade600,
-                                      Colors.red.shade700,
-                                      Colors.red.shade800,
-                                    ],
-            ) ,
-            borderRadius:BorderRadius.vertical(bottom:Radius.circular(30),
-
-
-            ), boxShadow: const [BoxShadow(color : Color(0xFF000000),   offset:Offset.zero,    blurRadius : 12.0,    spreadRadius : 0.0,  blurStyle :BlurStyle.normal)]
-        ) ,
-
-        child:Center(
-          child: SingleChildScrollView(
-
-              scrollDirection:Axis.horizontal,
-              child:Row(children: [
-            Text("GGGkmkmkrfvG"),
-            Text("GGGkmkmkrfvG"),
-            Text("GGGkmkmkrfvG"),
-            Text("GGGkmkmkrfvG"),
-            Text("GGGkmkmkrfvG"),
-            Text("GGGkmkmkrfvG"),
-            Text("GGGkmkmkrfvG"),
-            Text("GGGkmkmkrfvG"),
-
-
-          ],)),
-        ),
+           },
+           child: Row(children: [CircleAvatar(child: Icon(Icons.library_books),),Expanded(child: Text("Rachid bourigue")),CircleAvatar(child: Icon(Icons.library_books),)],)),],)
 
       ),
 
