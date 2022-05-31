e690da77bacc408bf22a624d8af5f92b493f9569
bourigue <rrbourigue@gmail.com>
2022-05-31 18:33:50 +0100

    testing atribute permaante in  get.put

--- lib/controller.dart
+++ lib/controller.dart
@@ -3,16 +3,19 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 
 class Controller extends GetxController{
 
-  var sum=0;
-  var num1=0;
-  var num2=0;
+
+  var num1=0.obs;
+  var num2=0.obs;
 
 
 
- increment(){num1++;update();}
+ increment(){num1++;}
 
- dincrement(){num2++;update();}
+ dincrement(){num2++;}
 
- summe(){sum=num1;update();}
+ int get summ=>num1.value+num2.value;
+
+
 
 }
+
--- lib/snackbar.dart
+++ lib/snackbar.dart
@@ -4,16 +4,16 @@ import 'package:get/get.dart';
 import 'controller.dart';
 
 void main(){
- runApp(GetMaterialApp(home: Home()));
+ runApp(GetMaterialApp(home: home()));
 }
 
 class Home extends StatelessWidget {
-
+ final Controller c=Get.put(Controller(),permanent: false);
   @override
   Widget build(context) {
 
     // Instantiate your class using Get.put() to make it available for all "child" routes there.
-  //  final Controller c = Get.put(Controller());
+    final Controller c = Get.put(Controller());
 
     return Scaffold(
       // Use Obx(()=> to update Text() whenever count is changed.
@@ -24,39 +24,45 @@ class Home extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GetBuilder<Controller>(
-
-            init:Controller(),
-            builder: (c){
-              print("num1 ${c.num1}");
-              return Text("${c.num1}");},),
-            GetBuilder<Controller>(
-
-              builder: (c){
-                print("num2 ${c.num2}");
-                return Text("${c.num2}");
-              } ,),
-            GetBuilder<Controller>(
-
-              builder: (c){
-                print("sum ${c.sum}");
-               return Text("${c.sum}");
-
-              },),
-            GetBuilder<Controller>(builder: (c) =>ElevatedButton(onPressed: (){ c.increment();c.summe();}, child: Text("+")),),
-            GetBuilder<Controller>(builder: (c) =>ElevatedButton(onPressed: (){ c.dincrement();c.summe();}, child: Text("-")),)
-
-
-
-
-            //   Obx(()=>Center(child:ElevatedButton.icon(onPressed: () { c.increment(); }, icon:Icon(Icons.add), label: Text("add"),))),
-            
-        ]
-
-
-           
-
-  ));
+            GetX<Controller>(init:Controller(), builder: (c){print("num1 ${c.num1}");return Text("${c.num1}");},),
+            GetX<Controller>(builder: (c){print("num2 ${c.num2}");return Text("${c.num2}");} ,),
+            GetX<Controller>(builder: (c){print("sum ${c.summ}");return Text("${c.summ}");},),
+            MaterialButton(onPressed: (){ c.increment();c.summ;}, child: Text("+")),
+            MaterialButton(onPressed: (){ c.dincrement();c.summ;}, child: Text("-")),
+            MaterialButton(onPressed: (){ Get.to(page2());}, child: Text("GO")),
+            //   Obx(()=>Center(child:MaterialButton.icon(onPressed: () { c.increment(); }, icon:Icon(Icons.add), label: Text("add"),))),
+          ]
+        ));
   }
 }
+class page2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     body: GetX<Controller>(builder: (c)=>Center(child: Text("${c.summ}"),),),
+    
+     
+   );
+  }
+  
+}
 
+class home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child: Column(children: [
+          MaterialButton(onPressed: (){ Get.to(page2());}, child: Text("page 2")),
+          MaterialButton(onPressed: (){ Get.to(Home());}, child: Text("page 1")),
+
+
+
+        ],),
+      ),
+
+
+    );
+  }
+
+}
